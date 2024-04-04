5ecbfd7ad934de53afc9c313b1099f09e3b42560
Maurice Vogel <10749869+mauricevogel@users.noreply.github.com>
2024-04-04 09:59:19 +0200

    Add state machine to `Subscription` model

--- test/models/subscription_test.rb
+++ test/models/subscription_test.rb
@@ -1,7 +1,33 @@
 require "test_helper"
 
 class SubscriptionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "within unpaid state, can transition to paid" do
+    subscription = subscriptions(:unpaid_subscription)
+
+    assert subscription.can_pay?
+    assert subscription.pay
+    assert subscription.paid?
+  end
+
+  test "within paid state, can transition to canceled" do
+    subscription = subscriptions(:paid_subscription)
+
+    assert subscription.can_cancel?
+    assert subscription.cancel
+    assert subscription.canceled?
+  end
+
+  test "within unpaid state, can not transition to canceled" do
+    subscription = subscriptions(:unpaid_subscription)
+
+    assert_not subscription.can_cancel?
+    assert_not subscription.cancel
+  end
+
+  test "when transitioning state, sets timestamp accordingly" do
+    subscription = subscriptions(:unpaid_subscription)
+    subscription.pay!
+
+    assert_not_nil subscription.paid_at
+  end
 end

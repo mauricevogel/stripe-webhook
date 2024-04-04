7ca829520a9211bc1dbd52218b714068e812266d
Maurice Vogel <10749869+mauricevogel@users.noreply.github.com>
2024-04-04 10:26:00 +0200

    Add processing of customer.subscription.created events

--- app/jobs/process_stripe_events_job.rb
+++ app/jobs/process_stripe_events_job.rb
@@ -2,6 +2,16 @@ class ProcessStripeEventsJob < ApplicationJob
   queue_as :default
 
   def perform(stripe_event)
-    # Process event
+    case stripe_event.event_type
+    when "customer.subscription.created"
+      process_subscription_created(stripe_event)
+    end
+  end
+
+  private
+
+  def process_subscription_created(stripe_event)
+    stripe_subscription = stripe_event.data["object"]
+    Subscription.create!(stripe_id: stripe_subscription["id"], stripe_customer_id: stripe_subscription["customer"])
   end
 end
--- test/jobs/process_stripe_events_job_test.rb
+++ test/jobs/process_stripe_events_job_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class ProcessStripeEventsJobTest < ActiveJob::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "when enqueued with a customer.subscription.created event, it creates a subscription record" do
+    stripe_event = stripe_events(:subscription_created_event)
+
+    assert_difference -> { Subscription.count }, 1 do
+      ProcessStripeEventsJob.perform_now(stripe_event)
+    end
+
+    assert_not_nil Subscription.find_by(stripe_id: stripe_event.data["object"]["id"])
+  end
 end

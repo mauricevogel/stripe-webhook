34bf81eb519cd6fe093db0aa1859d128b0999409
Maurice Vogel <10749869+mauricevogel@users.noreply.github.com>
2024-04-04 10:35:40 +0200

    Add processing of invoice.paid events

--- app/jobs/process_stripe_events_job.rb
+++ app/jobs/process_stripe_events_job.rb
@@ -5,10 +5,27 @@ class ProcessStripeEventsJob < ApplicationJob
     case stripe_event.event_type
     when "customer.subscription.created"
       process_subscription_created(stripe_event)
+    when "invoice.paid"
+      process_invoice_paid(stripe_event)
     end
   end
 
   private
+
+  def process_invoice_paid(stripe_event)
+    stripe_invoice = stripe_event.data["object"]
+    subscription = Subscription.find_by(stripe_id: stripe_invoice["subscription"])
+
+    # This will re-enqueue the job with a tiny offset if the subscription is not found yet.
+    # As Stripe can not guarantee the order of events coming in, we need to be prepared for a scenario
+    # where the invoice.paid event arrives before the customer.subscription.created event.
+    #
+    # Usually I would suggest a more thorough solution (e.g. fetching the subscription directly OR at least
+    # having an upper retry limit), but for the scope of the coding challenge here, I will keep it simple.
+    self.class.set(wait: 2.seconds).perform_later(stripe_event) and return if subscription.blank?
+
+    subscription.pay!
+  end
 
   def process_subscription_created(stripe_event)
     stripe_subscription = stripe_event.data["object"]
--- test/jobs/process_stripe_events_job_test.rb
+++ test/jobs/process_stripe_events_job_test.rb
@@ -10,4 +10,24 @@ class ProcessStripeEventsJobTest < ActiveJob::TestCase
 
     assert_not_nil Subscription.find_by(stripe_id: stripe_event.data["object"]["id"])
   end
+
+  test "when enqueued with an invoice.paid event, it marks the subscription as paid" do
+    stripe_event = stripe_events(:invoice_paid_event)
+    subscription = Subscription.create!(
+      stripe_id: stripe_event.data["object"]["subscription"],
+      stripe_customer_id: "cus_1"
+    )
+
+    assert_changes -> { subscription.reload.state }, from: "unpaid", to: "paid" do
+      ProcessStripeEventsJob.perform_now(stripe_event)
+    end
+  end
+
+  test "when enqueued with an invoice.paid event and the subscription is not found, it re-enqueues the job" do
+    stripe_event = stripe_events(:invoice_paid_event)
+
+    assert_enqueued_with(job: ProcessStripeEventsJob, args: [stripe_event]) do
+      ProcessStripeEventsJob.perform_now(stripe_event)
+    end
+  end
 end
